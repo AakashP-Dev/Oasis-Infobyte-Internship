6e05074c29a36830b21bbe5b14dc7611208dbde9
AakashP-Dev <your_work_email@example.com>
2026-08-07 18:58:29 +0530

    Added the Loops for Play Again Logic, and added total rounds played feature.

--- Aakash_2.java
+++ Aakash_2.java
@@ -5,20 +5,24 @@ class Aakash_2{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         GuessTheNumber GTN = new GuessTheNumber();
+        int result = 0;
+        int round = 1;
+        int loses = 0;
+        int wins = 0;
 
         System.out.println("+==================+");
         System.out.println("| GUESS THE NUMBER |");
         System.out.println("+==================+");
 
         System.out.println("\n+=============================================+");
-        System.out.print("Start the Game? Yes/No: ");
+        System.out.print("Play Game? Yes/No: ");
         String startGame = sc.next();
         System.out.println("+=============================================+");
 
         switch (startGame.toLowerCase()) {
             case "yes":
             case "y":
-                GTN.playGame();
+                result = GTN.playGame();
                 break;
             case "no":
             case "n":
@@ -30,6 +34,42 @@ class Aakash_2{
                 System.out.println("+=============================================+");
                 break;
         }
+
+        if(result==1){
+            wins++;            
+        }
+        else if(result==-1){
+            loses++;
+        }
+
+        while(true){
+            System.out.println("\n+=============================================+");
+            System.out.print("Want to Play Again? Yes/No: ");
+            String playAgain = sc.next();
+            System.out.println("+=============================================+");
+
+            if(playAgain.toLowerCase().equals("yes") || playAgain.toLowerCase().equals("y")) {
+                round++;
+                result = GTN.playGame();
+            }
+            else if(playAgain.toLowerCase().equals("no") || playAgain.toLowerCase().equals("n")) {
+                System.out.println("Your Played Total of " + round + " rounds");
+                System.out.println("Your Score is " + wins + " Wins and " + loses + " Loses.");
+                System.out.println("Thank you for playing!");
+                break;
+            }
+            else{
+                System.out.println("Invalid Input! Try again.");
+                System.out.println("+=============================================+");
+            }
+
+            if(result==1){
+            wins++;            
+            }
+            else if(result==-1){
+                loses++;
+            }
+        }
     }
 }
 
@@ -37,31 +77,42 @@ class GuessTheNumber{
     Scanner sc = new Scanner(System.in);
     Random rand = new Random();
 
-    public void playGame(){
+    public int playGame(){
         int randomNumber = rand.nextInt(100)+1;
-        System.out.println(randomNumber);
+        // System.out.println(randomNumber);
         int attemptCount = 1;
+        int gameStatus = 0;
+        
 
         while(true){
             System.out.println("Attempt no: " + attemptCount);
             System.out.print("Guess the Number(1-100): ");
             int guessedNumber = sc.nextInt();
 
-            if(guessedNumber == randomNumber){
-                System.out.println("Correct!");
-                System.out.println("You took " + attemptCount + " Attempts.");
-                System.out.println("+=============================================+");
+            if(attemptCount<=7){
+                if(guessedNumber == randomNumber){
+                    gameStatus = 1;
+                    System.out.println("Correct!");
+                    System.out.println("You took " + attemptCount + " Attempts.");
+                    System.out.println("+=============================================+");
+                    break;
+                }
+                else if(guessedNumber > randomNumber){
+                    System.out.println("Too High!");
+                    System.out.println("+=============================================+");
+                }
+                else if(guessedNumber < randomNumber){
+                    System.out.println("Too Low!");
+                    System.out.println("+=============================================+");
+                }
+            }else{
+                gameStatus = -1;
+                System.out.println("You Lose");
+                System.out.println("The Number was: " + randomNumber);
                 break;
-            }
-            else if(guessedNumber > randomNumber){
-                System.out.println("Too High!");
-                System.out.println("+=============================================+");
-            }
-            else if(guessedNumber < randomNumber){
-                System.out.println("Too Low!");
-                System.out.println("+=============================================+");
             }
             attemptCount++;
         }
+        return gameStatus;
     }
 }
